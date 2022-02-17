f57777c7ca25c90762eaa78404f412a959169a18
GitHub Action <action@github.com>
2022-02-17 09:45:10 +0000

    Template cleanup

--- src/main/kotlin/com/github/junjunlei/testideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/junjunlei/testideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.junjunlei.testideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/junjunlei/testideaplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/junjunlei/testideaplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.junjunlei.testideaplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.junjunlei.testideaplugin.MyBundle
 
 class MyApplicationService {
 
